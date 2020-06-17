2824b1d8ad01b31047e4d1a2d6360715974c9852
Naman Gupta <namanguptaa04@gmail.com>
2020-06-17 15:59:52 +0530

    App Java Things (with functionality) Pending UI modulation.

--- app/src/main/java/com/example/justjavathings/MainActivity.java
+++ app/src/main/java/com/example/justjavathings/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.justjavathings;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,34 +15,50 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-    int quantity = 0;
+    int quantity = 0, toppings = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
-    }
+        // Edit text object and input
+        EditText nameEditText = (EditText) findViewById(R.id.name_field);
+        String name = nameEditText.getText().toString();
+        Log.v("Main Activity", "Name: " + name);
 
+        // Whipped Cream checkbox object & input
+        CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
+
+        // chocolate checkbox object and input
+        CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.chocolate_cream_checkbox);
+        boolean hasChocolate = chocolateCheckbox.isChecked();
+
+        // log for whipped cream Verbose type
+        Log.v("MainActivity", "Has whipped Cream" + hasWhippedCream);
+
+        int orderPrice = calculatePrice(hasWhippedCream, hasChocolate);
+
+        // function call: createOrderSummary(args: name, price, haswhippedcream, haschocolate);
+        String priceMessage = createOrderSummary(name, orderPrice, hasWhippedCream, hasChocolate);
+        displayMessage(priceMessage);
+    }
     // This method is called when the plus button is clicked.
     public void increment(View view) {
-
         quantity = quantity + 1;
         display(quantity);
     }
-
     // This method is called when the order button is clicked.
     public void decrement(View view) {
-
         quantity = quantity - 1;
         display(quantity);
     }
-
     /**
      * This method displays the given quantity value on the screen.
      */
@@ -56,5 +75,42 @@ public class MainActivity extends AppCompatActivity {
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
 
+    /**
+     * Create summary of the order.
+     *
+     * @param hasWhippedCream is whether or not the user wants whipped cream topping
+     * @param hasChocolate    is whether or not the user wants chocolate topping
+     * @param price           of the order
+     * @return text summary
+     */
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        int price = 5;
+
+        if (addWhippedCream) {
+            price += 1;
+        }
+        if (addChocolate) {
+            price += 2;
+        }
+
+        return price * quantity;
+    }
+
+    private String createOrderSummary(String name, int price, boolean hasWhippedCream, boolean hasChocolate) {
+        String priceMessage = "Name: " + name;
+        priceMessage += "\n Add Chocolate?" + hasChocolate;
+        priceMessage += "\n Add Whipped Cream? " + hasWhippedCream;
+        priceMessage += "\n Quantity: " + quantity;
+        priceMessage += "\n Total: $" + price;
+        priceMessage += "\n Thank you!";
+        return priceMessage;
+    }
 }
